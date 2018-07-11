73d1abd61ba7fbe84cf1b33655992685415fc174
Fyery <chenfeier115121@163.com>
2018-07-11 11:55:44 +0800

    add user context

--- config/context.go
+++ config/context.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"context"
-
+	"github.com/pkg/errors"
 	"github.com/rancher/norman/controller"
 	"github.com/rancher/norman/event"
 	"github.com/rancher/norman/restwatch"
@@ -26,6 +26,11 @@ import (
 	"k8s.io/client-go/tools/record"
 	managementv3 "github.com/rancher/types/apis/management.cattle.io/v3"
 	managementSchema "github.com/rancher/types/apis/management.cattle.io/v3/schema"
+	appsv1beta2 "github.com/rancher/types/apis/apps/v1beta2"
+	extv1beta1 "github.com/rancher/types/apis/extensions/v1beta1"
+	batchv1 "github.com/rancher/types/apis/batch/v1"
+	batchv1beta1 "github.com/rancher/types/apis/batch/v1beta1"
+	knetworkingv1 "github.com/rancher/types/apis/networking.k8s.io/v1"
 )
 
 var (
@@ -162,6 +167,78 @@ func (c *ManagementContext) controllers() []controller.Starter {
 	}
 }
 
+type UserContext struct {
+	Management        *ManagementContext
+	ClusterName       string
+	RESTConfig        rest.Config
+	UnversionedClient rest.Interface
+	APIExtClient      clientset.Interface
+	K8sClient         kubernetes.Interface
+
+	Apps         appsv1beta2.Interface
+	Core         corev1.Interface
+	RBAC         rbacv1.Interface
+	Extensions   extv1beta1.Interface
+	BatchV1      batchv1.Interface
+	BatchV1Beta1 batchv1beta1.Interface
+	Networking   knetworkingv1.Interface
+}
+
+func (w *UserContext) controllers() []controller.Starter {
+	return []controller.Starter{
+		w.Apps,
+		w.Core,
+		w.RBAC,
+		w.Extensions,
+		w.BatchV1,
+		w.BatchV1Beta1,
+		w.Networking,
+	}
+}
+
+func (w *UserContext) UserOnlyContext() *UserOnlyContext {
+	return &UserOnlyContext{
+		Schemas:           w.Management.Schemas,
+		ClusterName:       w.ClusterName,
+		RESTConfig:        w.RESTConfig,
+		UnversionedClient: w.UnversionedClient,
+		K8sClient:         w.K8sClient,
+
+		Apps:         w.Apps,
+		Core:         w.Core,
+		RBAC:         w.RBAC,
+		Extensions:   w.Extensions,
+		BatchV1:      w.BatchV1,
+		BatchV1Beta1: w.BatchV1Beta1,
+	}
+}
+
+type UserOnlyContext struct {
+	Schemas           *types.Schemas
+	ClusterName       string
+	RESTConfig        rest.Config
+	UnversionedClient rest.Interface
+	K8sClient         kubernetes.Interface
+
+	Apps         appsv1beta2.Interface
+	Core         corev1.Interface
+	RBAC         rbacv1.Interface
+	Extensions   extv1beta1.Interface
+	BatchV1      batchv1.Interface
+	BatchV1Beta1 batchv1beta1.Interface
+}
+
+func (w *UserOnlyContext) controllers() []controller.Starter {
+	return []controller.Starter{
+		w.Apps,
+		w.Core,
+		w.RBAC,
+		w.Extensions,
+		w.BatchV1,
+		w.BatchV1Beta1,
+	}
+}
+
 func NewManagementContext(config rest.Config) (*ManagementContext, error) {
 	var err error
 
@@ -244,6 +321,108 @@ func (c *ManagementContext) Start(ctx context.Context) error {
 func (c *ManagementContext) StartAndWait() error {
 	ctx := signal.SigTermCancelContext(context.Background())
 	c.Start(ctx)
+	<-ctx.Done()
+	return ctx.Err()
+}
+
+func NewUserContext(scaledContext *ScaledContext, config rest.Config, clusterName string) (*UserContext, error) {
+	var err error
+	context := &UserContext{
+		RESTConfig:  config,
+		ClusterName: clusterName,
+	}
+
+	context.Management, err = scaledContext.NewManagementContext()
+	if err != nil {
+		return nil, err
+	}
+
+	context.K8sClient, err = kubernetes.NewForConfig(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = context.K8sClient.Discovery().ServerVersion()
+	if err != nil {
+		return nil, errors.Wrap(err, "could not contact server")
+	}
+
+	context.Apps, err = appsv1beta2.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	context.Core, err = corev1.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	context.RBAC, err = rbacv1.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	context.Networking, err = knetworkingv1.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	context.Extensions, err = extv1beta1.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	context.BatchV1, err = batchv1.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	context.BatchV1Beta1, err = batchv1beta1.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	dynamicConfig := config
+	if dynamicConfig.NegotiatedSerializer == nil {
+		configConfig := dynamic.ContentConfig()
+		dynamicConfig.NegotiatedSerializer = configConfig.NegotiatedSerializer
+	}
+
+	context.UnversionedClient, err = restwatch.UnversionedRESTClientFor(&dynamicConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	context.APIExtClient, err = clientset.NewForConfig(&dynamicConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return context, err
+}
+
+func (w *UserContext) Start(ctx context.Context) error {
+	logrus.Info("Starting cluster controllers for ", w.ClusterName)
+	controllers := w.Management.controllers()
+	controllers = append(controllers, w.controllers()...)
+	return controller.SyncThenStart(ctx, 5, controllers...)
+}
+
+func (w *UserContext) StartAndWait(ctx context.Context) error {
+	ctx = signal.SigTermCancelContext(ctx)
+	w.Start(ctx)
+	<-ctx.Done()
+	return ctx.Err()
+}
+
+func (w *UserOnlyContext) Start(ctx context.Context) error {
+	logrus.Info("Starting workload controllers")
+	return controller.SyncThenStart(ctx, 5, w.controllers()...)
+}
+
+func (w *UserOnlyContext) StartAndWait(ctx context.Context) error {
+	ctx = signal.SigTermCancelContext(ctx)
+	w.Start(ctx)
 	<-ctx.Done()
 	return ctx.Err()
 }
